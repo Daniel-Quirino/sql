cf5a77c8080bc9e0227352e0e80335b4e6e92d5e
Daniel Pires Quirino <danielpires6@hotmail.com>
2019-06-29 15:23:44 -0300

    database, table, insertInto and query

---
+++ BD/cursoEmVideo/inserInto.sql
@@ -0,0 +1,9 @@
+insert into pessoas
+(nome, nascimento,sexo, peso, altura, nacionalidade)
+values
+('Godofrego','1984-01-02','M','78.5','1.83','Brasil');
+
+
+insert into pessoas values
+(DEFAULT,'Maria','1999-12-30','F','55.5','1.65','Portugal'),
+(DEFAULT,'Tadeu','1939-11-30','M','65.5','2.05','Argentina');
